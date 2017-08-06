158285a60a0c528fecb376b16a142edf95e0deaa
chihiro <chanchi0707@gmail.com>
2017-08-07 03:33:10 +0900

    キー番号の取得

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,9 @@
+import $ from 'jquery';
+//こっからッスタート
+
 window.onload = function() {
 	console.log("始まるよ");
+	//一つめのデータ
 	let animData = {
 		wrapper: document.getElementById('bodymovin'),
 		animType: 'svg',
@@ -8,5 +12,26 @@ window.onload = function() {
 		autoplay: true,
 		path: '../json/vj1.json'
 	}
-	let anim = bodymovin.loadAnimation(animData);
+
+	//キー押した時のイベント
+	$("html").keydown(function(e){
+		switch(e.which){
+            case 65: // Key[→]
+            console.log("a");
+            break;
+ 
+            case 66: // Key[←]
+            console.log("b");
+            break;
+ 
+            case 67: // Key[↑]
+            console.log("c");
+            break;
+ 
+            case 68: // Key[↓]
+            console.log("d");
+            break;
+        }
+		// let anim = bodymovin.loadAnimation(animData);
+	});
 }
